cdf757fab176a592a34190ba393d99d00cf0d6f0
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-31 00:07:54 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,43 @@
 import React, { Component } from "react";
 import Clock from "./clock";
 class App extends Component {
-  render() {
-    return <Clock />;
+  class Clock {
+    constructor(props) {
+      super(props);
+      this.state = {
+        time: new Date().toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+          hour12: true
+        })
+      };
+    }
+    componentDidMount() {
+      this.timerId = setInterval(() => this.tick(), 1000);
+    }
+    componentWillUnmount() {
+      clearInterval(this.timerId);
+    }
+    tick() {
+      this.setState({
+        time: new Date().toLocaleTimeString("en-US", {
+          hour: "numeric",
+          minute: "numeric",
+          second: "numeric",
+          hour12: true
+        })
+      });
+    }
+    render() {
+      return (
+        <>
+        <div className="Clock">
+          <h3 id="time">{this.state.time}</h3>
+        </div>
+        </>
+      );
+    }
   }
-}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+function generateCodeFromObject(obj) {
+  //return a code generated string
+}
 
-ReactDOM.render(<App />, document.getElementById("root"));
+module.exports = generateCodeFromObject;
+
+// ReactDOM.render(<App />, document.getElementById("root"));
