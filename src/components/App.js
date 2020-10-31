0747781f24da32364e2ac1e5cc99f758de68879a
supriya-1234 <66170913+supriya-1234@users.noreply.github.com>
2020-10-31 21:29:23 +0530

    changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,39 @@
 import React, { Component } from "react";
-import Clock from "./clock";
 class App extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        time: new Date().toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-          hour12: true
-        })
-      };
-    }
-    componentDidMount() {
-      this.timerId = setInterval(() => this.tick(), 1000);
-    }
-    componentWillUnmount() {
-      clearInterval(this.timerId);
-    }
-    tick() {
-      this.setState({
-        time: new Date().toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-          second: "numeric",
-          hour12: true
-        })
-      });
-    }
-    render() {
-      return (
-        <div className="Clock">
-          <h3 id="time">{this.state.time}</h3>
-        </div>
-        
-      );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    };
   }
+  componentDidMount() {
+    this.timerId = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerId);
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleTimeString("en-US", {
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+        hour12: true
+      })
+    });
+  }
+  render() {
+    return (
+      <div className="Clock">
+        <h3 id="time">{this.state.time}</h3>
+      </div>
+    );
+  }
+}
 
 export default App;
